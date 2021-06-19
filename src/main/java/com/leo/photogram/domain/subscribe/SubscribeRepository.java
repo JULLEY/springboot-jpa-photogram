c8234f58b1f84f5c60d1700207fa9a37646cf448
woong <ung91@yahoo.com>
2021-06-20 00:35:17 +0900

    Subscribe model, repository 추가

---
+++ src/main/java/com/leo/photogram/domain/subscribe/SubscribeRepository.java
@@ -0,0 +1,7 @@
+package com.leo.photogram.domain.subscribe;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubscribeRepository extends JpaRepository<Subscribe, Integer> {
+
+}
